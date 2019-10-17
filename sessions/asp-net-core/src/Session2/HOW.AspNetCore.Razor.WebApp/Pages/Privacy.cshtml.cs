fa713ab8e270232b911b038a36934dee9ce1f52b
Kevin Kraus <kkraus@microsoft.com>
2019-10-17 13:53:43 -0600

    base session2 solution

---
+++ sessions/asp-net-core/src/Session2/HOW.AspNetCore.Razor.WebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HOW.AspNet.WebApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public string Title { get; set; }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
