17c0d66af7c912468bc47ae4cc1e4c9969e48882
Kevin Kraus <kkraus@microsoft.com>
2019-10-17 10:32:49 -0600

    updated keyvault config for prod env

--- sessions/asp-net-core/src/Session1/HOW.AspNetCore.WebApp/Program.cs
+++ sessions/asp-net-core/src/Session1/HOW.AspNetCore.WebApp/Program.cs
@@ -22,16 +22,17 @@ namespace HOW.AspNet.WebApp
                          if (context.HostingEnvironment.IsProduction())
                          {
                              var builtConfig = config.Build();
+                             var kvConfig = builtConfig.GetSection("AzureKeyVault");
 
                              using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
                              store.Open(OpenFlags.ReadOnly);
                              var certs = store.Certificates
                                  .Find(X509FindType.FindByThumbprint,
-                                     builtConfig["AzureADCertThumbprint"], false);
+                                     kvConfig["AzureADCertThumbprint"], false);
 
                              config.AddAzureKeyVault(
-                                 $"https://{builtConfig["AzureKeyVaultName"]}.vault.azure.net/",
-                                 builtConfig["AzureADApplicationId"],
+                                 $"https://{kvConfig["VaultName"]}.vault.azure.net/",
+                                 kvConfig["AzureADApplicationId"],
                                  certs.OfType<X509Certificate2>().Single());
 
                              store.Close();
