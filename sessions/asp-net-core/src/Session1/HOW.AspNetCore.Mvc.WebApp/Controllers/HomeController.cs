00b2a2f55d7912719a50b1a30892af0d92d1fef8
Kevin Kraus <kkraus@microsoft.com>
2019-10-31 14:26:17 -0400

    cleaned up messy stuff

--- sessions/asp-net-core/src/Session1/HOW.AspNetCore.Mvc.WebApp/Controllers/HomeController.cs
+++ sessions/asp-net-core/src/Session1/HOW.AspNetCore.Mvc.WebApp/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HOW.AspNetCore.Mvc.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using HOW.AspNetCore.Mvc.WebApp.Models;
+using System.Diagnostics;
 
 namespace HOW.AspNetCore.Mvc.WebApp.Controllers
 {
