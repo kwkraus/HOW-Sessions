6866cd0df5def4357213b5f80ce69ee5c0c07bd5
Kevin Kraus <kkraus@microsoft.com>
2019-11-22 20:08:24 -0700

    added DI exercise to demonstrate lifetime to MVC app

--- sessions/asp-net-core/src/Session4/HOW.AspNetCore.Mvc.WebApp/Startup.cs
+++ sessions/asp-net-core/src/Session4/HOW.AspNetCore.Mvc.WebApp/Startup.cs
@@ -1,10 +1,14 @@
 using HOW.AspNetCore.Data.Contexts;
+using HOW.AspNetCore.Services.Domains;
+using HOW.AspNetCore.Services.Interfaces;
+using HOW.AspNetCore.Services.Lifetime;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace HOW.AspNetCore.Mvc.WebApp
 {
@@ -24,6 +28,18 @@ namespace HOW.AspNetCore.Mvc.WebApp
 
             services.AddDbContext<HowDataContext>(options =>
                 options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HowAspNetCoreDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
+
+            services.AddTransient<IProductService, ProductService>();
+
+            //uncomment to demonstrate lifetime and registration options
+            services.AddTransient<IOperationTransient, Operation>();
+            services.AddScoped<IOperationScoped, Operation>();
+            services.AddSingleton<IOperationSingleton, Operation>();
+            services.AddSingleton<IOperationSingletonInstance>(new Operation(Guid.Empty));
+
+            // OperationService depends on each of the other Operation types.
+            services.AddTransient<OperationService, OperationService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
