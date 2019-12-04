30a46eba60b5029fbbb547fc2279792ba3ba3a97
Kevin Kraus <kkraus@microsoft.com>
2019-12-03 20:24:40 -0800

    fixed statuscode compile issue and upgraded to 3.1

---
+++ sessions/asp-net-core/src/Session5/HOW.AspNetCore.Mvc.WebApp/Models/StatusCodeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HOW.AspNetCore.Mvc.WebApp.Models
+{
+    public class StatusCodeViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string ErrorStatusCode { get; set; }
+
+        public string OriginalURL { get; set; }
+        public bool ShowOriginalURL => !string.IsNullOrEmpty(OriginalURL);
+    }
+}
