d89b823347806aed340b5c17407138c864fe6a29
Laurie Atkinson <laurieat@microsoft.com>
2020-02-11 00:09:52 -0700

    Add Authentication module

---
+++ sessions/angular/src/authentication_complete/src/app/services/auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
+import { AppConfig } from '../app.config';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuardService implements CanActivate {
+
+    constructor(private authService: AuthService) {
+    }
+
+    canActivate(): Promise<boolean> | boolean {
+        if (!AppConfig.settings.aad.requireAuth || this.authService.isUserAuthenticated) {
+            return true;
+        }
+        this.authService.login();
+        return false;
+    }
+}
---
+++ sessions/angular/src/bookservice_api/BookService/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using BookService.Models;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace BookService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add service and create Policy with options
+            services.AddCors(options =>
+            {
+            options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+                    //.AllowCredentials());
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "https://login.microsoftonline.com/7534f15c-6ed2-4469-bbb2-1d432dec01a6";
+                options.Audience = "5fb2a243-96aa-4583-abb7-a8a7e252cd1a";
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.DescribeAllEnumsAsStrings();
+                swagger.DescribeAllParametersInCamelCase();
+                swagger.SwaggerDoc("v1", new Info { Title = "Book Service API", Version = "v1" });
+            });
+
+            //todo: your connection string goes here pointing to a database that has the books. I have my sql database hosted on azure.
+            string connection = @"Server=tcp:dqclzb9t4a.database.windows.net,1433;Initial Catalog=BooksDB;Persist Security Info=False;User ID=code2joy;Password=Umyaya!2014;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<AngularWorkshopContext>(options => options.UseSqlServer(connection));
+            //services.AddDbContext<AngularWorkshopContext>(opt => opt.UseInMemoryDatabase("BooksDB"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
+            app.UseSwagger();
+            app.UseSwaggerUI(swagger =>
+            {
+                swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "Book Service API");
+            });
+
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<AngularWorkshopContext>();
+                context.Database.EnsureCreated();
+            }
+            app.UseMvc();
+        }
+    }
+}
